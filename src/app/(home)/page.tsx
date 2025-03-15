5af483ce431ce75804646d29807483607594ec60
nuhptr <nugrahaadi733@gmail.com>
2025-03-15 23:05:03 +0700

    feat: :hammer: update file in /app

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,22 @@
 import { HydrateClient, trpc } from "@/trpc/server"
-import { Suspense } from "react"
-import { ErrorBoundary } from "react-error-boundary"
 
-import { PageClient } from "./client"
+import { HomeView } from "@/modules/home/ui/views/home-view"
 
-export default async function Home() {
-    void trpc.hello.prefetch({ text: "client" })
+// make this page dynamic to any make change from server
+export const dynamic = "force-dynamic"
+
+interface PageProps {
+    searchParams: Promise<{ categoryId: string }>
+}
+
+export default async function Page({ searchParams }: PageProps) {
+    const { categoryId } = await searchParams
+
+    void trpc.categories.getMany.prefetch()
 
     return (
         <HydrateClient>
-            <Suspense fallback={<p>Loading....</p>}>
-                <ErrorBoundary fallback={<p>Error...</p>}>
-                    <PageClient />
-                </ErrorBoundary>
-            </Suspense>
+            <HomeView categoryId={categoryId} />
         </HydrateClient>
     )
 }
